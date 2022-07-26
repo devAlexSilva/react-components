4bd5ffc59a8ef0a5b5d51d94726cceea6df3aee3
Alex Silva <9apsilva@gmail.com>
2022-07-26 18:45:32 -0300

    style: added cards

--- modal/src/App.jsx
+++ modal/src/App.jsx
@@ -1,15 +1,57 @@
 import React, { useState } from 'react'
 import Modal from './components/Modal'
-
+import './App.scss'
 
 function App() {
 
   const [modalIsOpen, setModalIsOpen] = useState(false)
 
   return (
-    <div className="App">
-      <button onClick={() => setModalIsOpen(true)}>open modal</button>
-      {modalIsOpen ? <Modal onClose={() => setModalIsOpen(false)}><h2>modal is open</h2></Modal> : null}
+    <div className="main">
+      <ul className="container">
+        <li className="item">
+          <button
+            onClick={() => setModalIsOpen(true)}>
+            open modal
+          </button>
+        </li>
+        <li className="item">
+          <img src="https://cdn.myanimelist.net/images/anime/1408/114012.jpg?s=cccbe98f0a30d6c557aab125064f127d" alt="Akira" />
+          <div className="seemore">
+            <p>nome do anime</p>
+            <button
+              onClick={() => setModalIsOpen(true)}>
+              more
+            </button>
+
+          </div>
+        </li>
+        <li className="item">
+          <button
+            onClick={() => setModalIsOpen(true)}>
+            open modal
+          </button>
+        </li>
+      </ul>
+      {modalIsOpen ? (
+            <Modal
+              onClose={() => setModalIsOpen(false)}>
+              <h2>
+                modal is open
+              </h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Ut quaerat illum dolores deleniti voluptate nam impedit,
+                libero quidem tempore vel voluptas odio eos optio,
+                temporibus quisquam. Tenetur deleniti libero ad.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Ut quaerat illum dolores deleniti voluptate nam impedit,
+                libero quidem tempore vel voluptas odio eos optio,
+                temporibus quisquam. Tenetur deleniti libero ad.
+              </p>
+            </Modal>
+          ) : null
+          }
     </div>
   );
 }
--- modal/src/components/Modal.jsx
+++ modal/src/components/Modal.jsx
@@ -1,6 +1,11 @@
-function Modal({ onClose = () => {}, children }) {
+function Modal({ onClose = () => {}, children, id = 'modal' }) {
+    
+    const handleClick = (event) => {
+        event.target.id === 'modal' && onClose()
+    }
+    
     return (
-        <div className="modal">
+        <div id={id} className="modal" onClick={handleClick}>
             <div className="container">
                 <button className="close" onClick={onClose}/>
                 <div className="content">{children}</div>
