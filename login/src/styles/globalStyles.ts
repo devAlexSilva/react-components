8f8af2cebc7fbef1cc453191ede0f689b7ccac16
Alex Silva <9apsilva@gmail.com>
2022-10-01 13:30:20 -0300

    styles: added globalStyles

---
+++ login/src/styles/globalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-size: 16px;
+        background-color: #242424;
+        color: #f5f5f5;
+    }
+`
