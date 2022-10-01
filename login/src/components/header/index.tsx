cd86bd1933e533040e140c40ac42caa58211d3e8
Alex Silva <9apsilva@gmail.com>
2022-10-01 13:35:19 -0300

    feat: added header

---
+++ login/src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import * as S from "./styles";
+
+export const Header = () => {
+  return (
+    <>
+      <S.Container about="Header">
+        <S.Logo>Logo</S.Logo>
+        <S.Theme>switch</S.Theme>
+      </S.Container>
+    </>
+  );
+};
---
+++ login/src/components/header/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #20b2aa;
+    height: 3rem;
+    padding: .5rem;
+`
+
+export const Logo = styled.div`
+    text-transform: uppercase;
+    font-weight: 800;
+    font-family: sans-serif;
+    cursor: pointer;
+`
+
+export const Theme = styled.div`
+    
+`
